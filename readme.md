3c4d4d428595d0ab6387910170ebd3f9a4941479
Chris McDonald <mail.chris.mcdonald@gmail.com>
2018-01-10 12:53:36 -0700

    Added mutex, removed unsafety, updated benchmarks

--- readme.md
+++ readme.md
@@ -1,8 +1,7 @@
 My results for derangement calculations and a cached method.
 
 ```Rust
-test tests::bench_derange_cached ... bench:         155 ns/iter (+/- 13)
-test tests::bench_derange_fast   ... bench:       4,250 ns/iter (+/- 100)
-test tests::bench_derange_i      ... bench:       5,460 ns/iter (+/- 113)
-test tests::bench_derange_u      ... bench:       5,293 ns/iter (+/- 180)
+test tests::bench_derange_cached ... bench:         186 ns/iter (+/- 9)
+test tests::bench_derange_fast   ... bench:       3,832 ns/iter (+/- 169)
+test tests::bench_derange_i      ... bench:       5,289 ns/iter (+/- 259)
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 #![feature(test)]
+
 extern crate num;
 extern crate test;
+#[macro_use]
+extern crate lazy_static;
 
 use num::bigint::BigInt;
 
 use std::mem::replace;
 use std::collections::LinkedList;
+use std::sync::Mutex;
 
 pub fn derangements_i(mut i: i64) -> BigInt {
     match i {
@@ -24,22 +28,6 @@ pub fn derangements_i(mut i: i64) -> BigInt {
     }
 }
 
-pub fn derangements_u(mut i: u64) -> BigInt {
-    match i {
-        0 => BigInt::from(1),
-        1 => BigInt::from(0),
-        _ => {
-            let mut n1 = BigInt::from(0);
-            let mut n0 = BigInt::from(1);
-            while i != 0 {
-                let n2 = (i - 1) * (n0 + &n1);
-                n0 = replace(&mut n1, n2);
-                i = i - 1;
-            }
-            n0
-        }
-    }
-}
 
 pub fn derangements_fast(i: u32) -> BigInt {
     match i {
@@ -60,33 +48,34 @@ pub fn derangements_fast(i: u32) -> BigInt {
 }
 
 pub fn derangements_cached(x: usize) -> BigInt {
-    static mut STATIC_CACHE: Option<LinkedList<(usize, BigInt)>> = None;
-    unsafe {
-        let cache = STATIC_CACHE.get_or_insert_with(cache_init);
+    lazy_static!{
+        static ref STATIC_CACHE: Mutex<LinkedList<(usize, BigInt)>> = {
+            let mut cache = LinkedList::new();
+            cache.push_back((0, BigInt::from(1)));
+            cache.push_back((1, BigInt::from(0)));
+            Mutex::new(cache)
+        };
+    };
 
-        while cache.back().unwrap().0 < x {
-            let new: BigInt;
-            let index: usize;
-            {
-                let mut iter = cache.iter();
-                let &(i, ref n1) = iter.next_back().unwrap();
-                let &(_, ref n0) = iter.next_back().unwrap();
-                index = i + 1;
-                new = (n0 + n1) * i;
-            }
-            cache.push_back((index, new))
+
+    let mut cache = STATIC_CACHE.lock().unwrap();
+
+    while cache.back().unwrap().0 < x {
+        let new: BigInt;
+        let index: usize;
+        {
+            let mut iter = cache.iter();
+            let &(i, ref n1) = iter.next_back().unwrap();
+            let &(_, ref n0) = iter.next_back().unwrap();
+            index = i + 1;
+            new = (n0 + n1) * i;
         }
-
-        cache.iter().find(|&&(i, _)| i == x).unwrap().1.clone()
+        cache.push_back((index, new))
     }
+
+    cache.iter().find(|&&(i, _)| i == x).unwrap().1.clone()
 }
 
-fn cache_init() -> LinkedList<(usize, BigInt)> {
-    let mut cache = LinkedList::new();
-    cache.push_back((0, BigInt::from(1)));
-    cache.push_back((1, BigInt::from(0)));
-    cache
-}
 
 #[cfg(test)]
 mod tests {
@@ -96,11 +85,6 @@ mod tests {
     #[bench]
     fn bench_derange_i(b: &mut Bencher) {
         b.iter(|| derangements_i(64));
-    }
-
-    #[bench]
-    fn bench_derange_u(b: &mut Bencher) {
-        b.iter(|| derangements_u(64));
     }
 
     #[bench]
@@ -118,11 +102,7 @@ mod tests {
         let expected = BigInt::from(1334961);
         assert_eq!(derangements_fast(10), expected);
     }
-    #[test]
-    fn test_derangements_u() {
-        let expected = BigInt::from(1334961);
-        assert_eq!(derangements_u(10), expected);
-    }
+
     #[test]
     fn test_derangements_i() {
         let expected = BigInt::from(1334961);
